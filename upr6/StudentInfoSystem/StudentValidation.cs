ef393ac21cd006e43b0e82ef75253586f2a3e60f
AksayHasan <105709190+AksayHasan@users.noreply.github.com>
2022-05-17 11:47:16 +0300

    upr 6

---
+++ upr6/StudentInfoSystem/StudentValidation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserLogin;
+
+namespace StudentInfoSystem
+{
+    public class StudentValidation
+    {
+        public Student GetStudentDataByUser(User user)
+        {
+            StudentData studentData = new StudentData();
+            if (String.IsNullOrWhiteSpace(user.facNumber) || user == null)
+            {
+                Console.WriteLine("Faculty number is empty or there is no such student with this faculty number");
+                return null;
+            }
+
+            IEnumerable<Student> students = studentData.getStudents();
+
+            foreach (Student student in students)
+            {
+                if (student.FacultyNumber.Equals(user.facNumber))
+                {
+                    return student;
+                }
+            }
+
+            Console.WriteLine("No such student!");
+            return null;
+        }
+
+        public Student GetStudentDataByNameAndFacNum(string firstName,string facNumber)
+        {
+            StudentData studentData = new StudentData();
+
+            IEnumerable<Student> students = studentData.getStudents();
+
+            foreach (Student student in students)
+            {
+                if (student.FacultyNumber.Equals(facNumber) && student.FirstName.Equals(firstName))
+                {
+                    return student;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ upr6/UserLogin/Logs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    public class Logs
+    {
+
+        public int LogId { get; set; }
+        public string LogContent { get; set; }
+    }
+}
